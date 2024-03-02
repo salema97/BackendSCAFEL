ab1b91c723bf30cd4e6b48f1e4ed8202d5bee3a8
salema97 <HitsugayaYT@outlook.com>
2024-03-01 23:47:43 -0500

    Update TokenExpired for date

--- src/main/java/com/Scafel/ecom/utils/JwtUtil.java
+++ src/main/java/com/Scafel/ecom/utils/JwtUtil.java
@@ -1,15 +1,17 @@
 package com.Scafel.ecom.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -29,8 +31,34 @@ public class JwtUtil {
                 .signWith(getSignKey()).compact();
     }
 
-    private Key getSignKey(){
+    private SecretKey getSignKey() {
         byte[] keybytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keybytes);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(getSignKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 }
