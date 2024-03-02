9613c901f4dee09e9baf87feb685f85bd0513814
salema97 <HitsugayaYT@outlook.com>
2024-03-01 23:28:14 -0500

    Bug fixes createToken

--- src/main/java/com/Scafel/ecom/utils/JwtUtil.java
+++ src/main/java/com/Scafel/ecom/utils/JwtUtil.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,11 +22,11 @@ public class JwtUtil {
 
     private String createToken(Map<String, Object> claims, String userName) {
         return Jwts.builder()
-                .setClaims(claims)
-                .setSubject(userName)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 10000 * 60 * 30))
-                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+                .claims(claims)
+                .subject(userName)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 10000 * 60 * 30))
+                .signWith(getSignKey()).compact();
     }
 
     private Key getSignKey(){
